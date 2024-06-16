721e554d323639ff7d1f6600d3ea90d16275070a
育姍 李 <a22221883@gmail.com>
2024-06-16 19:23:11 +0800

    Model調整V0.1 -> 新增商品類、資料庫連線字串、安裝EntityFrameworkCore

---
+++ SunnyStore/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SunnyStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
